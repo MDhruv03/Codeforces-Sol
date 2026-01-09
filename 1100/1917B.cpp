a2d67e18323fdaddebc4ad25ec0a98e467e7dad1
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-01-09 14:32:24 +0530

    naur

--- 1100/1917B.cpp
+++ 1100/1917B.cpp
@@ -15,7 +15,21 @@ int main() {
 int tt;
   cin >> tt;
     while (tt--){
-
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int count = 0;
+        set<char> seen;
+        int result=0;
+        for(char c : s) {
+            if(seen.find(c) == seen.end()) {
+                seen.insert(c);
+                count++;
+            }
+            result+=count;
+        }
+        cout << result << endl;
 }
 
     return 0;
