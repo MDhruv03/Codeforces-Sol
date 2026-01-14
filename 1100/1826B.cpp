e3c0395a35d1c06beba5078183641383a9186a7f
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-01-14 11:11:45 +0530

    again

--- 1100/1826B.cpp
+++ 1100/1826B.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 #define ll long long
 #define endl '\n'
+ll gcd (ll a, ll b) 
+{ return b == 0 ? a : gcd(b, a % b); }
+
 
 int main() {
     ios::sync_with_stdio(false);
@@ -15,7 +18,21 @@ int main() {
     int tt = 1;
     cin >> tt;
     while (tt--) {
-        
+        int n;
+        cin >> n;
+        vector<ll> a(n);
+        for (int i=0;i<n;i++) cin >> a[i];
+        int l=0, r=n-1;
+
+        int num=0;
+        while(l<=r)
+        {
+            num =gcd(num, abs(a[r] - a[l]));
+            l++;
+            r--;
+        }
+        cout << num << endl;
+
     }
 
     return 0;
