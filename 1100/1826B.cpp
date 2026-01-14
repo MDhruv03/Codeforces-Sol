a6169cd679b78c4bb584921e9a0637ca3d942690
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-01-14 11:00:18 +0530

    yur

---
+++ 1100/1826B.cpp
@@ -0,0 +1,22 @@
+// Author: MDhruv03
+// Problem: 1826B
+// Time: 11:00 on 14/01/2026
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        
+    }
+
+    return 0;
+}
--- 1100/1827A.cpp
+++ 1100/1827A.cpp
@@ -1,22 +1,52 @@
 // Author: MDhruv03
-// Time: 10:25 on 14/01/2026
+// Time: 10:35 on 14/01/2026
 
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 #define endl '\n'
-
+const ll MOD = 1e9 + 7;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr);
 
-int tt;
-  cin >> tt;
-    while (tt--){
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        int n;
+        cin >> n;
+        vector<ll> a(n),b(n);
+
+        for (int i=0;i<n;i++) cin >> a[i];
+        for (int i=0;i<n;i++) cin >> b[i];
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        // 1 2 3 4 5 6 8 9
+        // 7 6 5 5 4 3 2 1 
         
-}   
+        ll ans = 1;
+        int j = n-1; 
+        int used=0;
+        for (int i = n-1; i >=0; i--) {
+            while (j >=0 && a[j] > b[i])
+                j--;
 
+            ll greaterCount = (n - 1) - j;
+            ll choices = greaterCount - used;
+
+            if (choices <= 0) {
+                ans = 0;
+                break;
+            }
+            ans = (ans * choices) % MOD;
+            used++;
+        }
+
+        cout << ans << '\n';
+    }
     return 0;
 }
