a6ade53deea0ff4dd9a459e789dc5a388ca39433
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-01-17 17:58:55 +0530

    wowzers

---
+++ 1200/tempCodeRunnerFile.cpp
@@ -0,0 +1,51 @@
+// Author: MDhruv03
+// Problem: 1909B
+// Time: 12:25 on 17/01/2026
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        int n;
+        cin >> n;
+        vector<ll> a(n);
+        for(int i=0;i<n;i++) cin >> a[i];
+
+        set<ll> st;
+        ll result;
+        for(int i =1;i<=60;i++)
+        {
+            ll temp = pow(2,i);
+            for(int i=0;i<n;i++)
+            {
+                if(st.find(a[i]%temp)==st.end())
+                    st.insert(a[i]%temp);
+                
+                if(st.size()>2)
+                {
+                    st.clear();
+                    break;
+                }
+                
+                if(st.size()==2)
+                {
+                    result=temp;
+                    break;
+                }
+            }
+        }
+
+        cout << result << '\n';
+    }
+
+    return 0;
+}
