11354af00547ae7299527ff2bea879c5e9d591e6
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-01-22 19:15:48 +0530

    better

--- 1200/1679B.cpp
+++ 1200/1679B.cpp
@@ -1,6 +1,6 @@
 // Author: MDhruv03
 // Problem: 1679B
-// Time: 22:02 on 21/01/2026
+// Time: 18:45 on 22/01/2026
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,30 +12,36 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tt = 1;
-    cin >> tt;
-    while (tt--) {
         ll n,q;
-        cin >> n >> q;
-        vector<ll> arr(n);
-        for(ll i=0;i<n;i++)
-            cin >> arr[i];
-        while(q--)
+        cin>>n>>q;
+        vector<pair<ll,ll>> arr(n,{0,0});
+        for(int i=0;i<n;i++) cin >> arr[i].first;
+
+        pair<ll,ll> globalValue = {0,-1};
+        ll sum=accumulate(arr.begin(),arr.end(),0LL,[](ll s,const pair<ll,ll>& p){ return s + p.first; });
+        for(int i=1;i<=q;i++)
         {
-            ll t , i x;
-            cin >> t >> i >> x;
-            if(t==1)
+            ll type;
+            cin>>type;
+            if(type==1)
             {
-                arr[i-1]=x;
-                ll sum = accumulate(arr.begin(), arr.end(), 0LL);
-                cout << sum << " " << '\n';
+                ll idx,x;
+                cin >> idx >> x;
+                idx--;
+                if(arr[idx].second > globalValue.second)
+                    sum += (x - arr[idx].first);
+                else
+                    sum += (x - globalValue.first);
+                arr[idx] = {x,i};    
             }
-            else
-            {
-                
+            else{
+                ll x;
+                cin >> x;
+                sum = n * x;
+                globalValue = {x,i};
             }
-        }    
-    }
+            cout << sum << '\n';
+        }
 
     return 0;
 }
