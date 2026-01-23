269e85b26bb67e4f7f42d7bbe690c7ae229ce663
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-01-23 18:44:24 +0530

    chad

--- 1200/1541B.cpp
+++ 1200/1541B.cpp
@@ -1,6 +1,6 @@
 // Author: MDhruv03
 // Problem: 1541B
-// Time: 11:05 on 23/01/2026
+// Time: 18:23 on 23/01/2026
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -15,23 +15,29 @@ int main() {
     int tt = 1;
     cin >> tt;
     while (tt--) {
-        int n;
-        cin >> n;
-
-        vector<int> arr(n);
-        for(int i=0;i<n;i++) cin >> arr[i];
-
-        int answer=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-                {
-                    if(arr[i] * arr[j] == i+j+2)
-                        answer++;
-                }
+        ll n;
+        cin >>n;
+        vector<pair<ll, ll>> a(n);
+        for(ll i=0;i<n;i++){
+            cin >> a[i].first;
+            a[i].second = i+1;
         }
 
-        cout << answer << '\n';
+        sort(a.begin(), a.end());
+        int pairs=0;
+        for(ll i=0;i<n;i++)
+        {
+            for( ll j=i+1;j<n;j++)
+            {
+                if(a[i].first * a[j].first >= 2*n)
+                    break;
+                if(a[i].first * a[j].first == a[i].second + a[j].second)
+                   pairs++;
+            }
+        }
+
+        cout << pairs << '\n';
+
     }
 
     return 0;
