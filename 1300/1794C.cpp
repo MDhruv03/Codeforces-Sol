c31626de7ad89e291522df160e193ed3d2c99e68
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-01-28 22:00:48 +0530

    i dont understand this whatsoever

---
+++ 1300/1794C.cpp
@@ -0,0 +1,22 @@
+// Author: MDhruv03
+// Problem: 1794C
+// Time: 21:59 on 28/01/2026
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        what the helly
+    }
+
+    return 0;
+}
