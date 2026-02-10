eb3403c148961f5f902d60fe20e8a605df369ed2
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-02-10 18:48:32 +0530

    you wouldnt say anything to me nor me to you

---
+++ 1400/1513B.cpp
@@ -0,0 +1,47 @@
+// Author: MDhruv03
+// Problem: 1513B
+// Time: 18:45 on 10/02/2026
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+
+const ll MOD=1e9+7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<ll> factorial(20,1);
+    for(ll i=1;i<20;i++)
+        factorial[i]=factorial[i-1]*i%MOD;
+
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for (ll i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        ll totalAnd=a[0];
+        for(ll i=1;i<n;i++)
+            totalAnd&=a[i];
+
+        ll countAnd=count(a.begin(),a.end(),totalAnd);
+        if(countAnd<2)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        ll ans=(countAnd*(countAnd-1))%MOD;
+        ans=(ans*factorial[n-2])%MOD;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
