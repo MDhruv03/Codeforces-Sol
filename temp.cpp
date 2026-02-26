da3831385c2b0ca2aaf13a1fc9df6d3d32e56155
Dhruv Maheshwari <dhruvmaheshwari2004@gmail.com>
2026-02-26 20:56:09 +0530

    since when was C this difficult

--- temp.cpp
+++ temp.cpp
@@ -1,6 +1,6 @@
 // Author: MDhruv03
 // Problem: temp
-// Time: 21:09 on 15/02/2026
+// Time: 20:40 on 26/02/2026
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,44 +8,71 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 
-
-bool intersect(vector<int> &a,vector<int> &b){
-    ll A=a[0]-b[0];
-    ll B=a[1]-b[1];
-    ll C=a[2]-b[2];
-
-    return B*B - 4*A*C >= 0;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 
     int tt = 1;
     cin >> tt;
     while (tt--) {
         int n;
         cin >> n;
-        vector<vector<int>> quad(n,vector<int>(3));
-        for(int i=0;i<n;i++){
-            cin >>quad[i][0]>>quad[i][1]>>quad[i][2];
-    }
-
-    map<int,int> count;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(!intersect(quad[i],quad[j])){
-                count[i]++;
-                count[j]++;
-            }
+        vector<vector<int>> blogs(n);
+        int totalL = 0;
+        for (int i = 0; i < n; ++i) {
+            int l; cin >> l;
+            totalL += l;
+            blogs[i].resize(l);
+            for (int j = 0; j < l; ++j) cin >> blogs[i][j];
         }
+        vector<vector<int>> R(n);
+        for (int i = 0; i < n; ++i) {
+            unordered_set<int> seen_in;
+            for (int j = (int)blogs[i].size() - 1; j >= 0; --j) {
+                int v = blogs[i][j];
+                if (!seen_in.count(v)) {
+                    R[i].push_back(v);   
+                    seen_in.insert(v);
+                }
+            }
+
+        }
+
+        vector<int> ans;
+        unordered_set<int> global_seen;
+        vector<char> used(n, 0);
+
+        while (true) {
+            int pick = -1;
+            vector<int> bestSeq;
+
+            for (int i = 0; i < n; ++i) {
+                if (used[i]) continue;
+                vector<int> cur;
+                cur.reserve(R[i].size());
+                for (int x : R[i]) {
+                    if (!global_seen.count(x)) cur.push_back(x);
+                }
+                if (cur.empty()) continue;
+                if (pick == -1 || cur < bestSeq) {
+                    pick = i;
+                    bestSeq = std::move(cur);
+                }
+            }
+
+            if (pick == -1) break;
+            for (int v : bestSeq) {
+                if (!global_seen.count(v)) {
+                    ans.push_back(v);
+                    global_seen.insert(v);
+                }
+            }
+            used[pick] = 1;
+        }
+        for (int v : ans) cout << v << ' ';
+        cout << '\n';
     }
 
-    for(int i=0;i<n;i++){
-        cout << count[i]+1 << " ";
-    }
-    cout << endl;
-
-}
     return 0;
 }
